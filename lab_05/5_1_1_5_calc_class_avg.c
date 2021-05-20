4304de7eba08af7935ad8a46cff5b3ee6ec8f873
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2021-05-20 16:49:12 +0200

    Corrected array intialization

--- lab_05/5_1_1_5_calc_class_avg.c
+++ lab_05/5_1_1_5_calc_class_avg.c
@@ -3,13 +3,19 @@
 # include "5_1_1_2_collect_marks.h"
 # include "5_1_1_3_arr_avg.h"
 # include "5_1_1_4_print_avg.h"
-
+/* 
+# include "5_1_1_1_num_students.c"
+# include "5_1_1_2_collect_marks.c"
+# include "5_1_1_3_arr_avg.c"
+# include "5_1_1_4_print_avg.c"
+ */
 int main() {
     int n_students;
-    float mark_arr[n_students];
     float avg;
 
     n_students = num_students();
+
+    float mark_arr[n_students];
 
     collect_marks(mark_arr, n_students);
 
