a39581e7df6b5f36ee90c96f45871e6f5c6c3005
freimuth <freimuth@uni-potsdam.de>
2021-05-26 21:05:30 +0200

    added outline of method

--- lab_05/5_2_2/5_2_2_binary_trees.c
+++ lab_05/5_2_2/5_2_2_binary_trees.c
@@ -11,7 +11,8 @@ typedef struct node {
 
 int main() {
 
-    // for arbitrary solution use an array storing 0 for missing nodes
+    // this array stores nodes from left to right, level after level
+    // it uses 0 for missing nodes
     int tree_array[MAX_LEVEL] = { 2, 9, 3, 7, 4, 0, 99 };
     
     int k = 0;
@@ -21,30 +22,25 @@ int main() {
 
     while (k < MAX_LEVEL) {
 
-        // find parent pointer
+        // TODO: find parent pointer    
+        /* for that find the index of the parent via floor(k/2) 
+            save left or right via k % 2, follow tree down to p
+            */    
 
-        
+        int l_r[lvl]; 
+        int p = k;
+
+        while (p != 0) {
+            
+        }
 
         node * curr_node = (node * ) malloc(sizeof(node));
 
         curr_node->value = tree_array[k];
+        curr_node->left_child = NULL;
+        curr_node->right_child = NULL;
 
-        int left_pos = 2 * k;
-        int right_pos = (2 * k) + 1;
-
-        // if we are at MAX_LEVEL, tree_array[left_post] 
-        // will be out of bounds
-        if (tree_array[left_pos] == 0 || lvl == MAX_LEVEL) {
-            curr_node->left_child = NULL;
-        } else {
-            curr_node->left_child = (node * ) malloc(sizeof(node));
-        }
-
-        if (tree_array[right_pos] == 0 || lvl == MAX_LEVEL) {
-            curr_node->right_child = NULL;
-        } else {
-            curr_node->right_child = (node * ) malloc(sizeof(node));
-        }
+        //TODO: add curr_node as child to parent pointer
 
         k ++;
     }
