21ff1c888aa45471810fe8c6c19411243c5fad4f
freimuth <freimuth@uni-potsdam.de>
2021-05-28 19:37:27 +0200

    started list filling function

--- lab_06/6_0_4/6_0_4_read_sort_sudent_table.c
+++ lab_06/6_0_4/6_0_4_read_sort_sudent_table.c
@@ -1,5 +1,6 @@
 # include <stdio.h>
 # include <stdlib.h>
+# include <string.h>
 
 # define MAX_NAME 100
 # define MAX_FILE 1000
@@ -13,16 +14,17 @@ typedef struct student {
 
 student * get_stud_list(char * infile);
 void print_file(char * infile);
-
+void print_stud_list(student * head);
 
 int main() {
     char f_path[] = "marks.tab";
 
-    print_file(f_path);
-/* 
+    // print_file(f_path);
+
     student * s_list = get_stud_list(f_path);
 
-     */
+    print_stud_list(s_list);
+    
     return (0);
 }
 
@@ -44,6 +46,23 @@ void print_file(char * infile) {
         printf("%s\t%s\t%s\n", first_name, last_name, mark);
 
     }
+
+    return;
+}
+
+void print_stud_list(student * head) {
+    
+    student * current;
+
+    current = head;
+
+    while (current != NULL) {
+        printf("%s\t%s\t%d\n", current->first_name, current->last_name,
+                current->mark);
+
+        current = current->next_student;
+    }
+
     return;
 }
 
@@ -56,5 +75,40 @@ student * get_stud_list(char * infile) {
         exit(1);
     }
 
+    char first_name[MAX_NAME];
+    char last_name[MAX_NAME];
+    char mark[MAX_NAME];
+    student * new, * current, * head;
+
+    head = NULL;
+
+    while(fscanf(f_pt, " %100[^\t]\t%100[^\t]\t%100[^\t\n]\n ", &first_name, &last_name, &mark) == 3) {       
+
+        printf("%s\t%s\t%s\n", first_name, last_name, mark);
+
+        double mark = (double) mark;
+
+        new = (student *) malloc(sizeof(student));
+        
+        strcpy(new->first_name, first_name);
+        strcpy(new->last_name, last_name);
+        new->mark = mark;
+        new->next_student = NULL;
+
+
+        // if this is the first student, perform additional steps
+        if (head == NULL) {
+            head = new;
+        } else {
+            /* if this is not the first student,
+             there already exists a current student, so we
+             can safely link there */
+            current->next_student = new;
+        }   
+
+        current = new;
+    }
+
+    return (head);
 
 }
