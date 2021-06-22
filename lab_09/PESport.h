88e78c24887b4e24d21ef8fc93e0ab32467489ad
freimuth <freimuth@uni-potsdam.de>
2021-06-22 15:49:31 +0200

    Moved static data into header files

--- lab_09/PESport.h
+++ lab_09/PESport.h
@@ -9,7 +9,7 @@ class PESport {
 
         int randint(int min, int max);
 
-        int pseudo2idx;
+        int pseudo2idx(int pseudo_idx);
 
         int get_rnd_aa_idx();
 
@@ -39,31 +39,43 @@ class PESport {
 
         // Coloring
         struct ANSII_col {
-            std::string reset;
-            std::string yellow;
-            std::string green;
-            std::string red;
+            std::string reset    = "\e[0m";
+            std::string yellow   = "\e[0;33m";
+            std::string green    = "\e[0;32m";
+            std::string red      = "\e[0;31m";
         };
 
+        ANSII_col cols;
+
         // Options
-        int maxTries;
-        int maxTime;
-        float tol;
+        int maxTries = 10;
+        int maxTime = 5;
+        float tol = 20;
 
         // Scoring system
         float score;
         time_t last_start_time;
 
+
         // Data and associated vars
-        std::array<std::string, 20> sl_code;
-        std::array<std::string, 20> tl_code;
+        std::array<std::string, 20> sl_code = { "A", "C", "D", "E", "F", "G", "H", "I", "K",
+                                    "L", "M", "N", "P", "Q", "R", "S", "T", "V",
+                                    "W", "Y" };
+        std::array<std::string, 20> tl_code = { "Ala", "Cys", "Asp", "Glu", "Phe", "Gly", "His", "Ile",
+                                    "Lys", "Leu", "Met", "Asn", "Pro", "Gln", 
+                                    "Arg", "Ser", "Thr", "Val", "Trp", "Tyr" };
         // according to this random website: https://www.webqc.org/aminoacids.php
-        std::array<float, 20> ml_mass;
+        std::array<float, 20> ml_mass = { 89.0935, 121.1590, 133.1032, 147.1299, 165.1900, 
+                                    155.1552, 131.1736, 146.1882, 131.1736,
+                                    149.2124, 132.1184, 115.1310, 146.1451, 
+                                    174.2017, 105.0930, 119.1197, 117.1469,
+                                    204.2262, 181.1894 };
 
-        int n_aminos;
+        int n_aminos = sizeof(sl_code) / sizeof(sl_code[0]);
 
-        std::array<bool, 20> aa_used;
-        int n_aa_used;
+        std::array<bool, 20> aa_used = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                    0, 0, 0, 0, 0 };
+        int n_aa_used = 0;
 
 };
 
--- lab_09/XPESport.h
+++ lab_09/XPESport.h
@@ -16,10 +16,14 @@ class XPESport : public PESport {
         std::string hdrpty2class(float hdrpty);
 
         void a2hydropathy();
-         
-        std::array<double, 20> hydropathys;
-        float hydrophobe_cut;
-        float neutral_cut;
+
+        // Order:   "A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N",
+        //          "P", "Q", "R", "S", "T", "V", "W", "Y"
+        std::array<double, 20> hydropathys = { 1.8, 2.5, -3.5, -3.5, 2.8, -0.4, -3.2,
+                                          4.5, -3.9, 3.8, 1.9, -3.5, -1.6, -3.5,
+                                          -4.5, -0.8, -0.7, 4.2, -0.9, -1.3 };
+        float hydrophobe_cut = -0.9;
+        float neutral_cut = -3.2;
 
 };
 
