7ea9cb1d26fd648937211eb14324986fad0380ba
freimuth <freimuth@uni-potsdam.de>
2021-07-13 15:30:19 +0200

    Task 3 working

--- lab_11/jf_FASTA_parser.cpp
+++ lab_11/jf_FASTA_parser.cpp
@@ -5,42 +5,57 @@
 # include <fstream>
 
 # include "argparse.hpp"
+# include "jf_utils.cpp"
 
 // function adapted from fasta-scan.cpp: scan()
-std::vector< std::pair<std::string,int> > scan (std::string filename) {
+std::vector< std::string > scan_pattern (std::string filename, std::string pattern) {
 
     std::ifstream infile; 
     std::string line;
     std::regex rxId("^>");
     std::string id;
-    int nchar = 0;
-    std::vector< std::pair<std::string,int> > result;
+    std::string sequence = "";
+    std::vector< std::string > result;
 
     infile.open(filename); 
 
+    // go through file
     while (std::getline(infile, line)) {
 
+        // check if we are at the start of a new sequence
         if (std::regex_search(line, rxId)) {
+            
+            // if we have a previous sequence do all the updating we need
+            if (sequence.length() > 0) {
+                
+                // if found, update result vector with id
+                if (jfutils::grepl(sequence, pattern)) {
+                    result.push_back(id);
+                }
 
-        if (nchar > 0) {
+            }
 
-            //std::cout << id << "\t" << nchar << std::endl;
-            result.push_back( { id, nchar } );
+            id = std::regex_replace(line, std::regex(">([^ ]+) *.*"), "$1");
 
-        }
-
-        id = std::regex_replace(line, std::regex(">([^ ]+) *.*"), "$1");
-        nchar = 0;
+            sequence = "";
 
         } else {
 
-        nchar += line.length();
+            sequence.append(line);
 
         }
     }
+            
+    // if we have a previous sequence do all the updating we need
+    if (sequence.length() > 0) {
+        
+        // if found, update result vector with id
+        if (jfutils::grepl(sequence, pattern)) {
+            result.push_back(id);
+        }
 
-    //std::cout << id << "\t" << nchar << std::endl;
-    result.push_back( { id, nchar } );
+    }
+
     infile.close();
 
     return(result);
@@ -48,6 +63,7 @@ std::vector< std::pair<std::string,int> > scan (std::string filename) {
 
 int main(int argc, char const *argv[]) {
 
+    using namespace std;
     namespace fs = std::filesystem;
     
     argparse::ArgumentParser parser("FASTA parser");
@@ -57,11 +73,10 @@ int main(int argc, char const *argv[]) {
         .required()
 
         // check right away if the given file exists
-        .action( [](const std::string& value) { 
+        .action( [](const string& value) { 
 
                 if (!fs::exists(value)) {
-                    std::cout << "File " << value << 
-                        " does not exist!\n";
+                    cout << "File " << value << " does not exist!\n";
 
                     exit(1);
                 }
@@ -82,7 +97,16 @@ int main(int argc, char const *argv[]) {
         exit(0);
     }
 
+    std::string infile = parser.get<std::string>("--infile");
+    std::string pattern = parser.get<std::string>("--pattern");
 
+    vector <string> ids = scan_pattern(infile, pattern);
+
+    cout << "IDs of sequences matching " << pattern << " are:\n";
+
+    for (auto i : ids) {
+        cout << "\t" << i << "\n";
+    }
 
     return 0;
 }
