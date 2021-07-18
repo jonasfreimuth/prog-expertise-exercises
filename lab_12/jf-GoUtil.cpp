98bbdd69fe47bccf30b5f2ab8a9f6ad018c4485d
freimuth <freimuth@uni-potsdam.de>
2021-07-18 19:55:51 +0200

    Tidied upfile, Three original functions working

--- lab_12/jf-GoUtil.cpp
+++ lab_12/jf-GoUtil.cpp
@@ -32,21 +32,7 @@ std::map<std::string, std::string> GoUtil::getEntry(std::string filename, std::s
 
         // check if we are at the entry matching the id
         if (std::regex_search(line, rxId)) {
-            std::cout << line << "\n";
             match = true;
-        }
-
-        if (match) {
-
-            // if (std::regex_search(line, splitField)) {
-
-                std::cout << "Key: " << std::regex_replace(line, splitField, "$1") <<
-                    "\n\tValue: " << std::regex_replace(line, splitField, "$2") << "\n";
-
-                entry.insert( 
-                    { std::regex_replace(line, splitField, "$1"),
-                      std::regex_replace(line, splitField, "$2") } );
-            // }
         }
 
         // check if we are done with the current entry
@@ -57,6 +43,13 @@ std::map<std::string, std::string> GoUtil::getEntry(std::string filename, std::s
                 // we dont need to look further
                 break;
             }
+        }
+
+        if (match) {
+
+            entry.insert( 
+                { std::regex_replace(line, splitField, "$1"),
+                    std::regex_replace(line, splitField, "$2") } );                    
         }
     }
 
@@ -72,26 +65,41 @@ std::string GoUtil::getName(std::string filename, std::string id) {
     entry = GoUtil::getEntry(filename, id);
 
     // TODO: Handle name not being in entry
-    name = entry["name"];   
+    name = entry["name"]; 
 
     return (name);
 };
 
 bool GoUtil::isObsolete(std::string filename, std::string id) {
-    std::cout << "Checking if obsolete...\n";
 
-    return (false);
+    std::map< std::string, std::string > entry = {}; 
+    std::string is_obsolete = "";
+    bool out = false;
+
+    entry = GoUtil::getEntry(filename, id);
+    is_obsolete = entry["is_obsolete"];
+
+    if (is_obsolete != "") {
+        out = true;
+    }
+
+    return (out);
 };
 
-int main(int argc, char const *argv[])
+/* int main(int argc, char const *argv[])
 {
     using namespace std;
     namespace fs = std::filesystem;
     
     GoUtil go_util = GoUtil();
 
-    auto entry  = go_util.getName("go.obo", "GO:0000001");
+    auto name  = go_util.getName("go.obo", "GO:0000001");
+    auto is_obsole  = go_util.isObsolete("go.obo", "GO:0000001");
+
+    cout << name << "\n";
+    cout << is_obsole << "\n";
+
 
     return 0;
-}
+} */
 
