863440d7f93edd5e7bea09b00e66f30f0252a6ca
freimuth <freimuth@uni-potsdam.de>
2021-07-25 19:54:54 +0200

    Added sorting for getN

--- py_test_exam_1/jf-FASTA-parser.cpp
+++ py_test_exam_1/jf-FASTA-parser.cpp
@@ -10,6 +10,9 @@
 std::vector< std::tuple< std::string, std::string, std::string > > jf_FASTA_parser::getN (std::string filename) {
 
     std::vector< std::tuple< std::string, std::string, std::string > > output = { };
+
+    std::map< std::string, int > seq_out = { };
+
     std::ifstream infile;
     std::string line = "";
     std::string seq = "";
@@ -23,7 +26,7 @@ std::vector< std::tuple< std::string, std::string, std::string > > jf_FASTA_pars
         if (std::regex_search(line, rxStart)) {
             id = std::regex_replace(line, rxId, "$1");
             if (seq.size() > 0) {
-                output.push_back( std::make_tuple( filename, id, std::to_string(seq.size()) ) );
+                seq_out[id] = seq.size();
                 seq = "";
             }
         }
@@ -31,6 +34,23 @@ std::vector< std::tuple< std::string, std::string, std::string > > jf_FASTA_pars
         if (! std::regex_search(line, rxStart)) {
             seq = seq + line;
         } 
+    }
+
+    if (seq.size() > 0) {
+        seq_out[id] = seq.size();
+        seq = "";
+    }
+
+    for (int i = 0; i < seq_out.size(); i ++) {        
+        // from https://stackoverflow.com/questions/30611709/find-element-with-max-value-from-stdmap#30611887:
+        auto max = std::max_element(seq_out.begin(), seq_out.end(),
+            [](const std::pair<std::string, int>& p1, const std::pair<std::string, int>& p2) {
+                return p1.second < p2.second; 
+            });
+
+        output.push_back( std::make_tuple( filename, max->first, std::to_string(max->second) ) );
+
+        max->second = 0;
     }
 
     infile.close();
